dfd3254e86ff68996de332bf0464a047c3712b6a
rodolfo_depaz <rodolfo_depaz>
2009-01-26 15:46:23 +0000

    support for cca, lqi and rssi

--- src/avrora/sim/radio/Medium.java
+++ src/avrora/sim/radio/Medium.java
@@ -48,15 +48,21 @@ import cck.util.Arithmetic;
  * reception of packets
  *
  * @author Ben L. Titzer
+ * @author Rodolfo de Paz
+ *
  */
 public class Medium {
 
     private static final int BYTE_SIZE = 8;
 
+    private static int Pn=-95;//Noise Power in dBm
+    private static double Pr=(double)Pn;//Received Power in dBm
     public interface Arbitrator {
-        public boolean lockTransmission(Receiver receiver, Transmission tran);
-        public char mergeTransmissions(Receiver receiver, List trans, long bit);
-    }
+        public boolean lockTransmission(Receiver receiver, Transmission tran,int Milliseconds);
+        public char mergeTransmissions(Receiver receiver, List trans, long bit, int Milliseconds);
+        public double computeReceivedPower(Medium.Transmission t, Medium.Receiver receiver, int Milliseconds);
+        public int getNoise(int index);
+    }    
    /**
      * The <code>Probe</code> interface defined method to insert and removes 
      * probes before and after transmit and receive.
@@ -178,11 +184,12 @@ public class Medium {
     *  The <code>beginTransmit</code> method creates a new transmission instatiating 
     *  <code>medium.newtransmission</code> and inserts a new ticker Event in 
     *  the simulator in a leadCycles time
-    *  @param pow power for the new transmission
+    *  @param pow power for the new transmission (dBm)
+    *  @param freq frequency for the new transmission (Mhz)
     */
-       public final void beginTransmit(double pow) {
+       public final void beginTransmit(double pow, double freq) {
             if ( !activated) {                
-                transmission = medium.newTransmission(this, pow);
+                transmission = medium.newTransmission(this, pow, freq);
                 activated = true;                
                 clock.insertEvent(ticker, leadCycles);
             }
@@ -243,32 +250,39 @@ public class Medium {
     public static abstract class Receiver extends TXRX {
         private static final int BIT_DELAY = 1;
         protected boolean locked;
-        public Receiver.Ticker ticker;
+        protected double frequency;
+        public Receiver.Ticker ticker;        
         //Receiver class constructor
         protected Receiver(Medium m, Clock c) {
             super(m, c);
-            ticker = new Ticker();
+            ticker = new Ticker();            
         }
         //Begin receiving. Insert a event.
-        public final void beginReceive() {
+        public final void beginReceive(double freq) {
+            frequency=freq;
 	    if (!activated) {
-		activated = true;           
+		activated = true;                
 		clock.insertEvent(ticker, leadCycles + cyclesPerByte);     
 	    }
         }
         //Ending reception. Remove event.
         public final void endReceive() {
-	    // Reception has been terminated, but check if receiver was still locked onto some transmission
-	    if (locked) {
-                nextByte(false, (byte)0);
-                if (probeList != null) probeList.fireAfterReceiveEnd(Receiver.this);
-	    }
+             // Reception has been terminated, but check if receiver was still locked onto some transmission
+             if (locked) {
+                 nextByte(false, (byte)0);
+                 if (probeList != null) probeList.fireAfterReceiveEnd(Receiver.this);
+             }
             activated = false;
             locked = false;
-            clock.removeEvent(ticker);
+            clock.removeEvent(ticker);            
         }
 
-        public abstract void nextByte(boolean lock, byte b);
+        public abstract byte nextByte(boolean lock, byte b);
+        
+        public abstract void setRssiValid(boolean v);
+        public abstract boolean getRssiValid();
+        public abstract void setRSSI(double rssi);
+        public abstract void setBER(double BER);
   /**
     *  The <code>Ticker</code> class implements a Simulator Event call Ticker
     *  that is fired  when a timed event occurs within the simulator in order 
@@ -340,12 +354,49 @@ public class Medium {
     */
             private void deliverByte(long oneBitBeforeNow) {
                 List it = getIntersection(oneBitBeforeNow - BYTE_SIZE);
-                if ( it != null ) {//there is a transmission 
-                    // merge transmissions into a single byte and send it to receiver
-                    char val = medium.arbitrator.mergeTransmissions(Receiver.this, it, oneBitBeforeNow - BYTE_SIZE);
-                    nextByte(true, (byte)val);
-                    if (probeList != null) probeList.fireAfterReceive(Receiver.this, val);
-                    clock.insertEvent(this, cyclesPerByte);
+                if ( it != null ) {//there is a transmission
+                    boolean one = false;                     
+                    double rssi=0.0;                    
+                    double SNR=0;
+                    double BER=0;                    
+                    assert it.size() > 0;     
+                    Iterator i = it.iterator();
+                        while ( i.hasNext() ) {                    
+                            Transmission t = (Transmission)i.next();
+                            if (one) {//more than one transmission                                        
+                                double I = medium.arbitrator.computeReceivedPower(t,Receiver.this,(int)clock.cyclesToMillis(clock.getCount()));
+                                //add interference to received power in linear scale
+                                rssi = 10*Math.log10(Math.pow(10,rssi/10) + Math.pow(10,I/10)); 
+                                SNR = SNR - I;
+                            } else {//only one transmission - no interference -
+                                one = true;                            
+                                Pr = medium.arbitrator.computeReceivedPower(t,Receiver.this,(int)clock.cyclesToMillis(clock.getCount()));
+                                Pn = medium.arbitrator.getNoise((int)clock.cyclesToMillis(clock.getCount()));
+                                rssi = Pr;
+                                SNR = Pr - Pn;
+                            }     
+                            double snr = Math.pow(10D,(SNR/10D));
+                            //ebno = snr / spectral efficiency = snr / log(1 + snr)
+                            double ebno = snr / Math.log(1+snr);
+                            //BER vs Ebno in AWGN channel
+                            double x = Math.sqrt(2*ebno);
+                            double x2 = Math.pow(x,2);
+                            BER=Math.exp(-x2/2)/(1.64D*x+Math.sqrt(0.76D*(x2)+4D));                        
+                            setBER(BER);
+                            setRSSI(rssi);
+                        }                   
+                        // merge transmissions into a single byte and send it to receiver
+                        // we return val in order to get rssi and corr value
+                        char val = medium.arbitrator.mergeTransmissions(Receiver.this, it, oneBitBeforeNow - BYTE_SIZE,(int)clock.cyclesToMillis(clock.getCount()));       
+                        //store high byte for corrupted bytes
+                        int hval = (int)(val>>>8);
+                        int value = (int) (0xff & nextByte(true, (byte)val));                    
+                        value |= (hval) & (value << 8);             
+                        value |=hval;
+                        val =(char)value;                        
+                        if (probeList != null) probeList.fireAfterReceive(Receiver.this, val);                                                    
+                        clock.insertEvent(this, cyclesPerByte);                      
+                       
                 } else {//no transmissions intersect
                     // all transmissions are over.
                     locked = false;
@@ -356,22 +407,52 @@ public class Medium {
             }
 
         }
-          /**
-    * The <code>isChannelClear</code> method determines wether the channel is clear  
+  /**
+    * The <code>isChannelClear</code> method determines wether the channel is clear 
     * or not 
     * @return true if channel is clear and false otherwise
     */
-        public final boolean isChannelClear() {
-            if (!activated) {//it is clear, return true
-                long time = clock.getCount();
-                long bit = getBitNum(time) - BIT_DELAY; // there is a one bit delay
-                waitForNeighbors(time - cyclesPerByte);
-                List it = getIntersection(bit - BYTE_SIZE);
-                return it != null;
-            } else {//it is activated return false
-                return !locked;
-            }
-        }
+        public final boolean isChannelClear(int RSSI_reg, int MDMCTRL0_reg) {            
+            //There are 3 modes (ED, 802.15.4 compliant detection, both)
+            int cca_mode = (MDMCTRL0_reg & 0x00c0) >>> 6;  
+                if (!activated) {//not receiving, CCA true depending on cca_mode
+                    long time = clock.getCount();
+                    long bit = getBitNum(time) - BIT_DELAY; // there is a one bit delay
+                    waitForNeighbors(time - cyclesPerByte);
+                    List it = getIntersection(bit - BYTE_SIZE);                              
+                    if ( it != null ) {//if there is a transmission compute rssi
+                        boolean one = false;  
+                        double rssi=0.0;                    
+                        assert it.size() > 0;     
+                        Iterator i = it.iterator();
+                            while ( i.hasNext() ) {                    
+                                Transmission t = (Transmission)i.next();
+                                if (one) {//more than one transmission                                        
+                                    double I = medium.arbitrator.computeReceivedPower(t,Receiver.this,(int)clock.cyclesToMillis(clock.getCount()));
+                                    //add interference to received power in linear scale
+                                    rssi = 10*Math.log10(Math.pow(10,rssi/10) + Math.pow(10,I/10));                                 
+                                } else {//only one transmission - no interference -
+                                    one = true;                            
+                                    Pr = medium.arbitrator.computeReceivedPower(t,Receiver.this,(int)clock.cyclesToMillis(clock.getCount()));
+                                    Pn = medium.arbitrator.getNoise((int)clock.cyclesToMillis(clock.getCount()));
+                                    rssi = Pr;                                
+                                }
+                            }
+                        //cca modes 1 and 3 compare threshold with rssi to determine CCA
+                        if (cca_mode == 1 | cca_mode == 3){
+                            int cca_hyst = (MDMCTRL0_reg & 0x0700) >>> 8;
+                            int cca_thr = ((RSSI_reg & 0xff00) >>> 8) - 256;
+                            int rssi_val = (int)rssi+45;            
+                            if (rssi_val < cca_thr - cca_hyst) return true;
+                            else return false;
+                        }//other modes true if no transmissions
+                        else return it != null;
+                    }else return it != null;//no transmissions CCA true
+                //receiving        
+                } else return !locked;//true if it is not locked onto a tx
+        }                   
+        
+        
   /**
     * The <code>earliestNewTransmission</code> method determines if there is a 
     * new transmission from the other threads
@@ -384,7 +465,7 @@ public class Medium {
                 Iterator i = medium.transmissions.iterator();
                 while ( i.hasNext() ) {
                     Transmission t = (Transmission)i.next();         
-                    if (bit <= t.firstBit && medium.arbitrator.lockTransmission(Receiver.this, t)) {
+                    if (bit <= t.firstBit && medium.arbitrator.lockTransmission(Receiver.this, t,(int)clock.cyclesToMillis(clock.getCount()))) {
                         if ( tx == null ) tx = t;
                         else if ( t.firstBit < tx.firstBit ) tx = t;
                     } else if (bit - 8 - 2 * medium.leadBits > t.lastBit) {
@@ -428,13 +509,14 @@ public class Medium {
         private void waitForNeighbors(long gtime) {
             if ( medium.synch != null ) medium.synch.waitForNeighbors(gtime);
         }
-    }
+    }   
+     
 
     public static class BasicArbitrator implements Arbitrator {
-        public boolean lockTransmission(Receiver receiver, Transmission trans) {
+        public boolean lockTransmission(Receiver receiver, Transmission trans, int Milliseconds) {
             return true;
         }
-        public char mergeTransmissions(Receiver receiver, List it, long bit) {
+        public char mergeTransmissions(Receiver receiver, List it, long bit, int Milliseconds) {
             assert it.size() > 0;
             Iterator i = it.iterator();
             Transmission first = (Transmission)i.next();
@@ -447,6 +529,12 @@ public class Medium {
             }
             return (char)value;
         }
+        public double computeReceivedPower(Medium.Transmission t, Medium.Receiver receiver, int Milliseconds){
+             return Pr;
+         }
+        public int getNoise(int index){
+             return Pn;
+         }
     }
 
     /**
@@ -460,6 +548,8 @@ public class Medium {
         public final long start;
         public final long firstBit;
         public final double power;
+        public final double Pt;
+        public final double f;
         public long lastBit;
         public long end;
 
@@ -473,9 +563,11 @@ public class Medium {
          * @param o Transmitter object
          * @param pow Power for the transmission
          **/
-        protected Transmission(Transmitter o, double pow) {
+        protected Transmission(Transmitter o, double pow, double freq) {
             origin = o;
             power = pow;
+            Pt = pow;
+            f = freq;
             start = o.clock.getCount();
             end = Long.MAX_VALUE;
             long l = start + o.leadCycles;
@@ -553,8 +645,8 @@ public class Medium {
      * @param p power for the new transmission
      * @return tx new transmission created
      */
-    protected synchronized Transmission newTransmission(Transmitter o, double p) {
-        Transmission tx = new Transmission(o, p);
+    protected synchronized Transmission newTransmission(Transmitter o, double p, double f) {
+        Transmission tx = new Transmission(o, p, f);
         transmissions.add(tx);
         return tx;
     }
